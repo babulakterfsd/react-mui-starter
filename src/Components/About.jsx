c4394d3b722889c2e440aee6ff36cdaf4e7973aa
Babul Akter <babulakterfsd@gmail.com>
2022-05-27 14:56:21 +0600

    about page updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import { Box, Container } from '@mui/material';
+import { styled } from '@mui/system';
+
+const MyComponent = styled('div')({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    padding: 8,
+    borderRadius: 4,
+  });
 
 
 const About = () => {
@@ -10,8 +19,13 @@ const About = () => {
            <Link to="/" style={{textDecoration: 'none'}}>
            <Button variant="contained">Back to Home Page</Button>
            </Link>
-
-
+           <Container style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', minHeight: '100vh'}}>
+              <Box>
+                 <MyComponent> 
+                     Hello Good Peoples !
+                 </MyComponent>
+              </Box>
+           </Container>
         </div>
     );
 };
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,22 @@
 import { Box, Button, Container, Typography } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home({mode}) {
+
   return (
-          <Container style={{display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh'}}>
+          <Container style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', minHeight: '100vh'}}>
               <Box>
                   <Typography>
                     The HomePage is in <Button variant='contained' color='beguni' style={{color: '#fff', padding: '5px'}}>
                     {mode === 'dark' ? 'Dark' : 'Light'}
                         </Button> Mode now.. 
                   </Typography>
+              </Box>
+              <Box>
+                  <Link to="/about" style={{color: '#fff', textDecoration: 'none'}}>
+                        <Button variant='contained' color='primary' style={{color: '#fff'}}> About Page</Button>
+                  </Link>
               </Box>
           </Container>
   )}
